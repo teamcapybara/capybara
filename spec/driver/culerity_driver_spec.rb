8706b7641c156cf31b7f2a20dc296305908fe9e8
Jonas Nicklas <jonas.nicklas@gmail.com>
2009-11-07 18:55:55 +0100

    Move driver specs into own directory

--- spec/driver/culerity_driver_spec.rb
+++ spec/driver/culerity_driver_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path('spec_helper', File.dirname(__FILE__))
+require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
 describe Webcat::Driver::Culerity do
   before do
--- spec/driver/safariwarit_driver_spec.rb
+++ spec/driver/safariwarit_driver_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path('spec_helper', File.dirname(__FILE__))
+require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
 describe Webcat::Driver::SafariWatir do
   before do
