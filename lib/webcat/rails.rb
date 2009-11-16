6e55c86a13bbe42a964dd4d3a6bd799b38dfbe93
Theo Hultberg and Jonas Nicklas <dev+iconara+jnicklas@elabs.se>
2009-11-16 20:13:32 +0100

    Serve static assets in Rails

--- lib/webcat/rails.rb
+++ lib/webcat/rails.rb
@@ -1,5 +1,11 @@
 require 'webcat'
 require 'webcat/dsl'
 
-Webcat.app = ActionController::Dispatcher.new
+Webcat.app = Rack::Builder.new do
+  map "/" do
+    use Rails::Rack::Static
+    run ActionController::Dispatcher.new
+  end
+end.to_app 
+
 Webcat.asset_root = Rails.root.join('public')
