c53e66309d01486a6f18de3977f1ca8dd07d5511
Theo Hultberg and Jonas Nicklas <dev+iconara+jnicklas@elabs.se>
2009-11-16 20:16:12 +0100

    Added some more cucumbery goodness

--- lib/webcat/cucumber.rb
+++ lib/webcat/cucumber.rb
@@ -2,3 +2,19 @@ require 'webcat'
 require 'webcat/dsl'
 
 World(Webcat)
+
+After do
+  Webcat.reset_sessions!
+end
+
+require 'database_cleaner'
+require 'database_cleaner/cucumber'
+DatabaseCleaner.strategy = :truncation
+
+Before('@javascript') do
+  Webcat.current_driver = Webcat.javascript_driver
+end
+
+After('@javascript') do
+  Webcat.use_default_driver
+end
