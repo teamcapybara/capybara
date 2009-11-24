56d5c2cd4415c8299b9fd6fcc98294621507e114
Jonas Nicklas <jonas.nicklas@gmail.com>
2009-11-24 23:37:25 +0100

    Completely comment out Watir driver specs
    So they don't load and require their dependencies

--- spec/driver/safariwarit_driver_spec.rb
+++ spec/driver/safariwarit_driver_spec.rb
@@ -1,10 +1,10 @@
 require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
-describe Capybara::Driver::SafariWatir do
-  before do
-    @driver = Capybara::Driver::SafariWatir.new(TestApp)
-  end
-  
-  # it_should_behave_like "driver"
-  # it_should_behave_like "driver with javascript support"
-end
+# describe Capybara::Driver::SafariWatir do
+#   before do
+#     @driver = Capybara::Driver::SafariWatir.new(TestApp)
+#   end
+#   
+#   it_should_behave_like "driver"
+#   it_should_behave_like "driver with javascript support"
+# end
