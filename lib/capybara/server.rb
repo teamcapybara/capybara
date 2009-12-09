61338d0786e64adf8d78c10831c3480fc67e6e5d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-12-09 16:39:03 +0100

    Handle absolute URIs (happens with Rails)

--- lib/capybara/server.rb
+++ lib/capybara/server.rb
@@ -1,3 +1,4 @@
+require 'uri'
 require 'net/http'
 require 'rack'
 require 'rack/handler/mongrel'
@@ -18,6 +19,7 @@ class Capybara::Server
   end
 
   def url(path)
+    path = URI.parse(path).request_uri if path =~ /^http/
     "http://#{host}:#{port}#{path}"
   end
   
