dba9a0421c4a7f99d77f9761424d7489fdc799cd
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2010-02-24 09:25:47 +0100

    Support both Rails 2 and Rails 3

--- lib/capybara/rails.rb
+++ lib/capybara/rails.rb
@@ -3,8 +3,13 @@ require 'capybara/dsl'
 
 Capybara.app = Rack::Builder.new do
   map "/" do
-    ActionDispatch::Static
-    run Rails.application  
+    if Rails.version.to_f >= 3.0
+      ActionDispatch::Static
+      run Rails.application  
+    else # Rails 2
+      use Rails::Rack::Static
+      run ActionController::Dispatcher.new
+    end
   end
 end.to_app
 
