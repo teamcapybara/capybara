58380cce3d2121fbafd7e21ddd6969a56508a176
Steven Parkes <smparkes@smparkes.net>
2010-02-25 16:32:24 -0800

    revert spec chanage; disallow file urls again

--- spec/dsl/current_url_spec.rb
+++ spec/dsl/current_url_spec.rb
@@ -2,7 +2,7 @@ shared_examples_for "current_url" do
   describe '#current_url' do
     it "should return the current url" do
       @session.visit('/form')
-      @session.current_url.should =~ %r((file|http)://[^/]*/form)
+      @session.current_url.should =~ %r(http://[^/]+/form)
     end
   end
 end
