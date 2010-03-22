1ff9ac2ab76f5e70e2b3d6a058388f2010e8e08f
Jonas Nicklas <jonas.nicklas@gmail.com>
2010-03-22 23:50:07 +0000

    tagged 0.3.6

--- lib/capybara.rb
+++ lib/capybara.rb
@@ -2,7 +2,7 @@ require 'timeout'
 require 'nokogiri'
 
 module Capybara
-  VERSION = '0.3.5'
+  VERSION = '0.3.6'
 
   class CapybaraError < StandardError; end
   class DriverNotFoundError < CapybaraError; end
