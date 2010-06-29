9dad4fe75531afc67a3b6f4d2531b6f6f10b63e3
Jonas Nicklas <jonas.nicklas@gmail.com>
2010-06-30 00:21:49 +0200

    More informative error message for infinite redirects

--- lib/capybara/driver/rack_test_driver.rb
+++ lib/capybara/driver/rack_test_driver.rb
@@ -248,7 +248,7 @@ class Capybara::Driver::RackTest < Capybara::Driver::Base
     5.times do
       follow_redirect! if response.redirect?
     end
-    raise Capybara::InfiniteRedirectError, "infinite redirect detected!" if response.redirect?
+    raise Capybara::InfiniteRedirectError, "redirected more than 5 times, check for infinite redirects." if response.redirect?
   end
 
 private
