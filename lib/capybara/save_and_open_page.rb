e6ae5d7234f759649884b525e17eabbc4fc9df22
Jonas Nicklas <jonas.nicklas@gmail.com>
2010-06-29 21:54:23 +0200

    No assuming active support!

--- lib/capybara/save_and_open_page.rb
+++ lib/capybara/save_and_open_page.rb
@@ -5,7 +5,7 @@ module Capybara
     def save_and_open_page(html)
       name = File.join(*[Capybara.save_and_open_page_path, "capybara-#{Time.new.strftime("%Y%m%d%H%M%S")}.html"].compact)
 
-      unless Capybara.save_and_open_page_path.blank? || File.directory?(Capybara.save_and_open_page_path )
+      unless Capybara.save_and_open_page_path.nil? || File.directory?(Capybara.save_and_open_page_path )
         FileUtils.mkdir_p(Capybara.save_and_open_page_path)
       end
       FileUtils.touch(name) unless File.exist?(name)
