38e74453b5f44c08442739038f636fcb762cf40a
Jonas Nicklas <jonas.nicklas@gmail.com>
2010-07-03 21:28:49 +0200

    Actually, we don't seem to need to do the static stuff at all in Rails 3

--- lib/capybara/rails.rb
+++ lib/capybara/rails.rb
@@ -4,7 +4,6 @@ require 'capybara/dsl'
 Capybara.app = Rack::Builder.new do
   map "/" do
     if Rails.version.to_f >= 3.0
-      use ActionDispatch::Static, Rails.public_path
       run Rails.application  
     else # Rails 2
       use Rails::Rack::Static
