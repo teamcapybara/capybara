7fc12fd78ea3bf642236cf62315ec73f4272a567
Jonas Nicklas <jonas.nicklas@gmail.com>
2010-07-29 15:20:11 +0200

    Drivers are now manually registered

--- spec/capybara_spec.rb
+++ spec/capybara_spec.rb
@@ -14,5 +14,16 @@ describe Capybara do
       Capybara.default_wait_time.should == 5
     end
   end
+
+  describe '.register_driver' do
+    it "should add a new driver" do
+      Capybara.register_driver :schmoo do |app|
+        Capybara::Driver::RackTest.new(app)
+      end
+      session = Capybara::Session.new(:schmoo, TestApp)
+      session.visit('/')
+      session.body.should include("Hello world!")
+    end
+  end
   
 end
