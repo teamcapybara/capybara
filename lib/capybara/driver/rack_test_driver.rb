d6760e4d86271e8a916ca8a8257ae02c4fae7a5e
Philip Arndt <parndt@gmail.com>
2010-11-01 17:33:41 +1300

    We need to exclude file inputs with multiple (HTML5) i.e. <input type='file' name='image[file][]' multiple /> from this check because it will cast the value to_s which totally kills file uploads when inside an array (array is required for multiple files).

--- lib/capybara/driver/rack_test_driver.rb
+++ lib/capybara/driver/rack_test_driver.rb
@@ -113,7 +113,7 @@ class Capybara::Driver::RackTest < Capybara::Driver::Base
     def params(button)
       params = {}
 
-      native.xpath(".//input[not(@disabled) and (not(@type) or (@type!='radio' and @type!='checkbox' and @type!='submit' and @type!='image'))]").map do |input|
+      native.xpath(".//input[not(@disabled) and (not(@type) or (@type!='radio' and not(@type='file' and @multiple) and @type!='checkbox' and @type!='submit' and @type!='image'))]").map do |input|
         merge_param!(params, input['name'].to_s, input['value'].to_s)
       end
       native.xpath(".//textarea[not(@disabled)]").map do |textarea|
