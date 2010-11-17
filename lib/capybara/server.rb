09d6335986780ac4a9794a7111bc710af423ff01
Adam Cigánek <adam.ciganek@gmail.com>
2010-11-17 16:40:39 +0100

    Server boot timeout is now configurable

--- lib/capybara/server.rb
+++ lib/capybara/server.rb
@@ -65,7 +65,9 @@ module Capybara
             Capybara.server.call(Identify.new(@app), @port)
           end
 
-          Capybara.timeout(10) { if responsive? then true else sleep(0.5) and false end }
+          Capybara.timeout(Capybara.server_boot_timeout) do
+            if responsive? then true else sleep(0.5) and false end
+          end
         end
       end
     rescue TimeoutError
