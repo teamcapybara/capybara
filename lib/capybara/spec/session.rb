dae0ad8679d6186156a2745032ac1f341f169dce
Jo Liss <joliss42@gmail.com>
2011-02-17 23:55:16 +0100

    Added Session#html as alias for Session#body, and some documentation

--- lib/capybara/spec/session.rb
+++ lib/capybara/spec/session.rb
@@ -34,6 +34,16 @@ shared_examples_for "session" do
     end
   end
 
+  describe '#html' do
+    it "should return the unmodified page body" do
+      # html and body should be aliased, but we can't just check for
+      # method(:html) == method(:body) because these shared examples get run
+      # against the DSL, which uses forwarding methods.  So we test behavior.
+      @session.visit('/')
+      @session.body.should include('Hello world!')
+    end
+  end
+
   describe '#source' do
     it "should return the unmodified page source" do
       @session.visit('/')
