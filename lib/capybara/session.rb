218510e64f2fa8c2a2ccd3a709897be5dbfd1b93
Jonas Nicklas <jonas.nicklas@gmail.com>
2011-03-25 14:22:47 +0100

    Explicit delegate instead of method_missing

--- lib/capybara/session.rb
+++ lib/capybara/session.rb
@@ -26,14 +26,18 @@ module Capybara
   # When using capybara/dsl, the Session is initialized automatically for you.
   #
   class Session
-    DSL_METHODS = [
-      :all, :first, :attach_file, :body, :html, :text, :check, :choose, :click_link_or_button, :click_button, :click_link, :current_url, :current_host,
-      :evaluate_script, :field_labeled, :fill_in, :find, :find_button, :find_by_id, :find_field, :find_link, :has_content?, :has_css?,
-      :has_no_content?, :has_no_css?, :has_no_xpath?, :has_xpath?, :save_page, :save_and_open_page, :select, :source, :uncheck,
-      :visit, :wait_until, :within, :within_fieldset, :within_table, :within_frame, :within_window, :has_link?, :has_no_link?, :has_button?,
+    NODE_METHODS = [
+      :all, :first, :attach_file, :text, :check, :choose, :click_link_or_button, :click_button, :click_link,
+      :field_labeled, :fill_in, :find, :find_button, :find_by_id, :find_field, :find_link, :has_content?, :has_css?,
+      :has_no_content?, :has_no_css?, :has_no_xpath?, :has_xpath?, :save_page, :save_and_open_page, :select, :uncheck,
+      :has_link?, :has_no_link?, :has_button?,
       :has_no_button?, :has_field?, :has_no_field?, :has_checked_field?, :has_unchecked_field?, :has_no_table?, :has_table?,
-      :unselect, :has_select?, :has_no_select?, :current_path, :has_selector?, :has_no_selector?, :click_on
+      :unselect, :has_select?, :has_no_select?, :has_selector?, :has_no_selector?, :click_on, :has_no_checked_field?, :has_no_unchecked_field?
     ]
+    SESSION_METHODS = [
+      :body, :html, :current_url, :current_host, :evaluate_script, :source, :visit, :wait_until, :within, :within_fieldset, :within_table, :within_frame, :within_window, :current_path
+    ]
+    DSL_METHODS = NODE_METHODS + SESSION_METHODS
 
     attr_reader :mode, :app
 
@@ -276,12 +280,12 @@ module Capybara
       Capybara::Node::Document.new(self, driver)
     end
 
-    def method_missing(*args)
-      current_node.send(*args)
-    end
-
-    def respond_to?(method)
-      super || current_node.respond_to?(method)
+    NODE_METHODS.each do |method|
+      class_eval <<-RUBY
+        def #{method}(*args, &block)
+          current_node.send(:#{method}, *args, &block)
+        end
+      RUBY
     end
 
   private
