97da26eef40d751e97b68500ebbb94d82227307e
Jon Leighton <j@jonathanleighton.com>
2011-05-13 13:22:50 +0100

    The at_exit hook to quit the selenium browser should not execute for unrelated child processes

--- spec/driver/selenium_driver_spec.rb
+++ spec/driver/selenium_driver_spec.rb
@@ -12,4 +12,15 @@ describe Capybara::Selenium::Driver do
   it_should_behave_like "driver with support for window switching"
   it_should_behave_like "driver without status code support"
   it_should_behave_like "driver with cookies support"
+
+  it "should not interfere with forking child processes" do
+    # Launch a browser, which registers the at_exit hook
+    browser = Capybara::Selenium::Driver.new(TestApp).browser
+
+    # Fork an unrelated child process. This should not run the code in the at_exit hook.
+    pid = fork { "child" }
+    Process.wait2(pid)[1].exitstatus.should == 0
+
+    browser.quit
+  end
 end
