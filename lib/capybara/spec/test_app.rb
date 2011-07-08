6f010783f1e4beab9dbe9c337727dc4a25c09ba0
Chris Lowder <clowder@gmail.com>
2011-07-08 16:15:52 +0100

    Fix a bug in the test app, this also exposes a false positive in the test suite.

--- lib/capybara/spec/test_app.rb
+++ lib/capybara/spec/test_app.rb
@@ -95,7 +95,7 @@ class TestApp < Sinatra::Base
   end
 
   post '/upload_empty' do
-    if params[:form][:file].nil?
+    if params[:form] && params[:form][:file].nil?
       'Successfully ignored empty file field.'
     else
       'Something went wrong.'
