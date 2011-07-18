0f273905b9519558e81f7a125fff8f7653c69ed7
Jeremy Hinegardner <jeremy@hinegardner.org>
2011-07-17 18:58:46 -0600

    Update to launchy-2.0

--- lib/capybara/util/save_and_open_page.rb
+++ lib/capybara/util/save_and_open_page.rb
@@ -23,7 +23,7 @@ module Capybara
 
     def open_in_browser(path) # :nodoc
       require "launchy"
-      Launchy::Browser.run(path)
+      Launchy.open(path)
     rescue LoadError
       warn "Sorry, you need to install launchy (`gem install launchy`) and " <<
         "make sure it's available to open pages with `save_and_open_page`."
