e286bc6f71e805df0c1284c379126d41d160bc73
Chris Lowder <clowder@gmail.com>
2011-07-22 14:00:48 +0100

    Refactor the driver to make it more readable.

--- lib/capybara/mechanize/driver.rb
+++ lib/capybara/mechanize/driver.rb
@@ -1,5 +1,3 @@
-require 'rack/test'
-require 'rack/utils'
 require 'mechanize'
 
 class Capybara::Mechanize::Driver < Capybara::Driver::Base
@@ -11,7 +9,7 @@ class Capybara::Mechanize::Driver < Capybara::Driver::Base
     @rack_server = Capybara::Server.new(@app)
     @rack_server.boot if Capybara.run_server
   end
-  
+
   def browser
     unless @browser
       @browser = Mechanize.new
@@ -20,39 +18,22 @@ class Capybara::Mechanize::Driver < Capybara::Driver::Base
     @browser
   end
 
-  def visit(path)  
+  def visit(path)
     browser.get(url(path))
   end
-  
+
   def follow(_method, path, attributes={})
-    # Don't go anywhere if the path ends with a hash (i.e. JS links).
-    return if path.gsub(/^#{request_path}/, '').start_with?('#')
-    
-    absolute_url = if URI.parse(path).relative?
-                     # If we have no path append the current path.
-                     path = "/#{ request_path }#{ path }" if path.start_with?('?')
-                     
-                     if current_page? && (current_page.uri.host != (Capybara.app_host || rack_server.host))
-                       new_uri      = current_page.uri.clone
-                       new_uri.path = path
-                       
-                       new_uri.to_s
-                     else
-                       url(path)
-                     end
-                   else
-                     path
-                   end
-                   
-    # Make sure that we append the current page as the referer for 
-    # GET requests.
+    return if is_javascript_hashy_path?(path)
+
+    absolute_url = make_absolute_url(path)
+
     if _method == :get
       browser.get(absolute_url, [], self.current_url)
     else
-      browser.send(_method, absolute_url, attributes)      
+      browser.send(_method, absolute_url, attributes)
     end
   end
-  
+
   def submit(form_node, button_node)
     form   = Capybara::Mechanize::Form.new(form_node, browser)
     button = Mechanize::Form::Button.new(button_node)
@@ -61,7 +42,7 @@ class Capybara::Mechanize::Driver < Capybara::Driver::Base
 
     browser.submit(form, button)
   end
-      
+
   def current_url
     current_page? ? current_page.uri.to_s : ''
   end
@@ -82,7 +63,7 @@ class Capybara::Mechanize::Driver < Capybara::Driver::Base
     current_page? ? current_page.body : ''
   end
   alias :body :source
-  
+
   def dom
     current_page? ? current_page.root : Nokogiri::HTML(nil)
   end
@@ -90,23 +71,48 @@ class Capybara::Mechanize::Driver < Capybara::Driver::Base
   def reset!
     @browser = nil
   end
-    
+
   private
-  
+
   def url(path)
     rack_server.url(path)
   end
-  
+
   def current_page
     browser.current_page
   end
-    
+
   def current_page?
     !browser.current_page.nil?
   end
-  
+
+  def current_page_is_external?
+    current_page.uri.host != (Capybara.app_host || rack_server.host)
+  end
+
   def request_path
     browser.current_page.uri.path
   end
-  
+
+  def make_absolute_url(path)
+    return path unless URI.parse(path).relative?
+
+    path = "/#{ request_path }#{ path }" if is_query_string?(path)
+
+    if current_page? && current_page_is_external?
+       new_uri      = current_page.uri.clone
+       new_uri.path = path
+       new_uri.to_s
+     else
+       url(path)
+     end
+  end
+
+  def is_query_string?(path)
+    path.start_with?('?')
+  end
+
+  def is_javascript_hashy_path?(path)
+    path.gsub(/^#{request_path}/, '').start_with?('#')
+  end
 end
