f6b3124d7fa67be14042f5bb6c3aa28ea8701e10
Edgars Beigarts <edgars.beigarts@makit.lv>
2011-08-22 12:32:35 +0300

    Renamed rspec examples

--- spec/fixtures/selenium_driver_rspec_success.rb
+++ spec/fixtures/selenium_driver_rspec_success.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Capybara::Selenium::Driver do
-  it "should exit with a non-zero exit status when assertion fails" do
+  it "should exit with a zero exit status" do
     browser = Capybara::Selenium::Driver.new(TestApp).browser
     true.should == true
   end
