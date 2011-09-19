bd1524508b64d97964b482e7dfae026978d66d09
Steve Schwartz <steve@alfajango.com>
2011-09-19 16:50:58 -0400

    Added Rails 3.1 asset pipeline support to #save_and_open_page

--- lib/capybara/util/save_and_open_page.rb
+++ lib/capybara/util/save_and_open_page.rb
@@ -32,11 +32,35 @@ module Capybara
     def rewrite_css_and_image_references(response_html) # :nodoc:
       root = Capybara.asset_root
       return response_html unless root
-      directories = Dir.new(root).entries.select { |name|
-        (root+name).directory? and not name.to_s =~ /^\./
+      entries = Dir.new(root).entries
+      directories = []
+      files = []
+      entries.each { |name|
+        if (root+name).directory? and not name.to_s =~ /^\./
+          directories << name
+        else
+          files << name
+        end
       }
       if not directories.empty?
         response_html.gsub!(/("|')\/(#{directories.join('|')})/, '\1' + root.to_s + '/\2')
+      end
+      if (not files.empty?) and defined?(::Rails) and ::Rails.version >= "3.1" and ::Rails.application.config.assets.enabled
+        response_html = rewrite_assets_from_asset_pipeline(response_html)
+      end
+      return response_html
+    end
+
+    def rewrite_assets_from_asset_pipeline(response_html)
+      asset_prefix = ::Rails.application.config.assets.prefix
+      public_asset_path = File.join(::Rails.public_path, asset_prefix)
+      manifest = File.join(public_asset_path, "manifest.yml")
+      unless Dir.exists?(public_asset_path) && File.exists?(manifest)
+        raise "You must first run `rake assets:precompile`"
+      end
+      assets    = YAML::load(File.open(manifest))
+      assets.each do |name, asset_name|
+        response_html.gsub!(name, asset_name)
       end
       return response_html
     end
