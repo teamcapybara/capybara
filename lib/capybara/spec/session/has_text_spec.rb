6337a1cc12334ba67314e3ac221b168199e9e196
Mark Dodwell <mark@mkdynamic.co.uk>
2011-10-16 21:45:24 -0700

    fix typo leading to false positive in RackTest driver (wip)

--- lib/capybara/spec/session/has_text_spec.rb
+++ lib/capybara/spec/session/has_text_spec.rb
@@ -54,12 +54,12 @@ shared_examples_for 'has_text' do
     end
 
     it 'should be false if text is in the title tag in the head' do
-      @session.visit('/with_html')
+      @session.visit('/with_js')
       @session.should_not have_text('with_js')
     end
 
     it 'should be false if text is inside a script tag in the body' do
-      @session.visit('/with_html')
+      @session.visit('/with_js')
       @session.should_not have_text('a javascript comment')
       @session.should_not have_text('aVar')
     end
@@ -115,12 +115,12 @@ shared_examples_for 'has_text' do
     end
 
     it 'should be true if text is in the title tag in the head' do
-      @session.visit('/with_html')
+      @session.visit('/with_js')
       @session.should have_no_text('with_js')
     end
 
     it 'should be true if text is inside a script tag in the body' do
-      @session.visit('/with_html')
+      @session.visit('/with_js')
       @session.should have_no_text('a javascript comment')
       @session.should have_no_text('aVar')
     end
