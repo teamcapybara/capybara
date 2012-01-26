4329a3bee323681da093b85d405b9afbf15bcb71
Jo Liss <joliss42@gmail.com>
2012-01-26 22:05:49 +0100

    Add workaround for "docElement is null" flickering test problem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,21 @@ RSpec.configure do |config|
       config.default_selector = :xpath
     end
   end
+
+  # Workaround for http://code.google.com/p/selenium/issues/detail?id=3147:
+  # Rerun the example if we hit a transient "docElement is null" error
+  config.around(:each) do |example|
+    attempts = 0
+    begin
+      example.run
+      e = @example.instance_variable_get('@exception') # usually nil
+      if (e.is_a?(Selenium::WebDriver::Error::UnknownError) &&
+          e.message == 'docElement is null' && (attempts += 1) < 5)
+        @example.instance_variable_set('@exception', nil)
+        redo
+      end
+    end until true
+  end
 end
 
 # Required here instead of in rspec_spec to avoid RSpec deprecation warning
