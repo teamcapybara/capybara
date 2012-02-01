cd04365ce70fc2c60c4d22e8809d7bdcd02e5646
Jonas Nicklas <jonas.nicklas@gmail.com>
2012-02-01 16:13:41 +0100

    Initial commit

---
+++ config.rb
@@ -0,0 +1,13 @@
+# Require any additional compass plugins here.
+require 'ninesixty'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "src"
+images_dir = "images"
+javascripts_dir = "javascripts"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = false
+preferred_syntax = :sass
+output_style = :compact
