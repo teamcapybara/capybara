f0dc3fdf4f5802938c3c50e1d48b0a1c2beae22c
Cezary Baginski <cezary.baginski@gmail.com>
2012-02-18 06:53:05 +0100

    handle cucumber 1.1.5 api change

--- lib/capybara/cucumber.rb
+++ lib/capybara/cucumber.rb
@@ -16,7 +16,13 @@ Before '@javascript' do
 end
 
 Before do |scenario|
-  scenario.source_tag_names.each do |tag|
+  tags = begin
+    scenario.source_tags.map(&:name) # Cucumber >= 1.1.5
+  rescue NoMethodError
+    scenario.source_tag_names
+  end
+
+  tags.each do |tag|
     driver_name = tag.sub(/^@/, '').to_sym
     if Capybara.drivers.has_key?(driver_name)
       Capybara.current_driver = driver_name
