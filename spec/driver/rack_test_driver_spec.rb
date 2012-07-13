1be49963e65621efb48b07cc43f333729338b90c
Jonas Nicklas <jonas.nicklas@gmail.com>
2012-07-13 13:01:35 +0200

    WTF does that do???

--- spec/driver/rack_test_driver_spec.rb
+++ spec/driver/rack_test_driver_spec.rb
@@ -2,18 +2,6 @@
 require 'spec_helper'
 require 'stringio'
 
-def capture(*streams)
-  streams.map! { |stream| stream.to_s }
-  begin
-    result = StringIO.new
-    streams.each { |stream| eval "$#{stream} = result" }
-    yield
-  ensure
-    streams.each { |stream| eval("$#{stream} = #{stream.upcase}") }
-  end
-  result.string
-end
-
 describe Capybara::RackTest::Driver do
   before do
     @driver = TestSessions::RackTest.driver
