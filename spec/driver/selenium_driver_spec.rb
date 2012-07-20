c51cc137f4b373a9730144d0cb9c2d3bfbf18c1b
Jo Liss <joliss42@gmail.com>
2012-07-20 15:57:17 +0200

    Remove forking test. Closes #763
    This causes "file not added for reaping" errors with Selenium, as the
    exit handler is called twice.
    
    Thanks @jarib for tracking this one down!

--- spec/driver/selenium_driver_spec.rb
+++ spec/driver/selenium_driver_spec.rb
@@ -14,19 +14,6 @@ describe Capybara::Selenium::Driver do
   it_should_behave_like "driver without status code support"
   it_should_behave_like "driver with cookies support"
 
-  unless Config::CONFIG['host_os'] =~ /mswin|mingw/
-    it "should not interfere with forking child processes" do
-      # Launch a browser, which registers the at_exit hook
-      browser = Capybara::Selenium::Driver.new(TestApp).browser
-
-      # Fork an unrelated child process. This should not run the code in the at_exit hook.
-      pid = fork { "child" }
-      Process.wait2(pid)[1].exitstatus.should == 0
-
-      browser.quit
-    end
-  end
-
   describe "exit codes" do
     before do
       @current_dir = Dir.getwd
