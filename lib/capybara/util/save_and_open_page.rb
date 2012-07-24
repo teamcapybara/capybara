54dbf744d4dd71aeb4b2a19d6511f1fa1f9672f4
Jonas Nicklas <jonas.nicklas@gmail.com>
2012-07-24 08:56:00 +0200

    Simplify save_and_open_page
    No more asset rewriting, since it doesn't work anyway

--- lib/capybara/util/save_and_open_page.rb
+++ lib/capybara/util/save_and_open_page.rb
@@ -1,45 +1,21 @@
 module Capybara
   class << self
-    def save_page(html, file_name=nil)
-      file_name ||= "capybara-#{Time.new.strftime("%Y%m%d%H%M%S")}#{rand(10**10)}.html"
-      name = File.join(*[Capybara.save_and_open_page_path, file_name].compact)
+    def save_page(html, path=nil)
+      path ||= "capybara-#{Time.new.strftime("%Y%m%d%H%M%S")}#{rand(10**10)}.html"
+      path = File.expand_path(path, Capybara.save_and_open_page_path) if Capybara.save_and_open_page_path
 
-      unless Capybara.save_and_open_page_path.nil? || File.directory?(Capybara.save_and_open_page_path )
-        FileUtils.mkdir_p(Capybara.save_and_open_page_path)
-      end
-      FileUtils.touch(name) unless File.exist?(name)
+      FileUtils.mkdir_p(File.dirname(path))
 
-      tempfile = File.new(name,'w')
-      tempfile.write(rewrite_css_and_image_references(html))
-      tempfile.close
-      tempfile.path
+      File.open(path,'w') { |f| f.write(html) }
+      path
     end
 
     def save_and_open_page(html, file_name=nil)
-      open_in_browser save_page(html, file_name)
-    end
-
-  protected
-
-    def open_in_browser(path) # :nodoc
-      require "launchy" # could raise LoadError
-      raise LoadError unless Launchy::Version::MAJOR >= 2
+      require "launchy"
+      path = save_page(html, file_name)
       Launchy.open(path)
     rescue LoadError
-      warn "You need launchy >=2.0.0 to open pages with `save_and_open_page`:\n" <<
-        "gem 'launchy', '>= 2.0.0', :require => false, :group => :test"
-    end
-
-    def rewrite_css_and_image_references(response_html) # :nodoc:
-      root = Capybara.asset_root
-      return response_html unless root
-      directories = Dir.new(root).entries.select { |name|
-        (root+name).directory? and not name.to_s =~ /^\./
-      }
-      if not directories.empty?
-        response_html.gsub!(/("|')\/(#{directories.join('|')})/, '\1' + root.to_s + '/\2')
-      end
-      return response_html
+      warn "Please install the launchy gem to open page with save_and_open_page"
     end
   end
 end
