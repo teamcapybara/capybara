b2fede28bfb9a471a9e9ca2aef8d67c4bc0872d6
Jonas Nicklas <jonas.nicklas@gmail.com>
2012-10-30 14:30:06 +0100

    Remove workaround for "docElement is null" error
    The bug seems to be fixed in Selenium, and it works for me locally, so let's try and see how we do without this.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,24 +6,6 @@ require "bundler/setup"
 require 'rspec'
 require 'capybara'
 
-RSpec.configure do |config|
-  # Workaround for http://code.google.com/p/selenium/issues/detail?id=3147:
-  # Rerun the example if we hit a transient "docElement is null" error
-  config.around(:each) do |example|
-    attempts = 0
-    begin
-      example.run
-      # example is just a Proc, @example is the current RSpec::Core::Example
-      e = @example.instance_variable_get('@exception') # usually nil
-      if (defined?(Selenium::WebDriver::Error::UnknownError) && e.is_a?(Selenium::WebDriver::Error::UnknownError) &&
-          e.message == 'docElement is null' && (attempts += 1) < 5)
-        @example.instance_variable_set('@exception', nil)
-        redo
-      end
-    end until true
-  end
-end
-
 require 'capybara/spec/spec_helper'
 
 module TestSessions
