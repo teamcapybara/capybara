41b46253d3dfd75e5f9afc484bcd1d1d024ed3e8
Jon Rowe <hello@jonrowe.co.uk>
2013-03-05 11:10:04 +1100

    ensure that the content editable element has focus by selecting an empty selection

--- lib/capybara/selenium/node.rb
+++ lib/capybara/selenium/node.rb
@@ -39,6 +39,13 @@ class Capybara::Selenium::Node < Capybara::Driver::Node
         #workaround for selenium raising an ElementNotVisible on empty elements
         driver.browser.execute_script "arguments[0].innerHTML = '&nbsp;'", native
       end
+      #ensure we are focused on the element
+      script = <<-JS
+        var range = document.createRange();
+        range.setStart(arguments[0].childNodes[0], 0);
+        window.getSelection().addRange(range);
+      JS
+      driver.browser.execute_script script, native
       native.clear
       native.send_keys(value.to_s)
     end
