716a86cedb8b35394e325ef222f8d71f8c3d0ed6
arirusso <ari.russo@gmail.com>
2015-01-06 17:59:31 -0500

    naming convention

--- lib/capybara-patch.rb
+++ lib/capybara-patch.rb
@@ -1,10 +1,10 @@
 require "capybara"
 require "wait"
 
-require "capybara-patch/idempotent_browser_quit"
-require "capybara-patch/synchronization"
-require "capybara-patch/threaded_server"
-require "capybara-patch/whitespace"
+require "capybara_patch/idempotent_browser_quit"
+require "capybara_patch/synchronization"
+require "capybara_patch/threaded_server"
+require "capybara_patch/whitespace"
 
 module CapybaraPatch
 
