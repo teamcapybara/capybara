e9577901de9aec9ab35eefbe3832fc19e07bcda9
arirusso <ari.russo@gmail.com>
2015-01-06 17:37:39 -0500

    require patch modules

--- lib/capybara-patch.rb
+++ lib/capybara-patch.rb
@@ -1,1 +1,7 @@
-require 'capybara'
+require "capybara"
+require "wait"
+
+require "capybara-patch/idempotent_browser_quit"
+require "capybara-patch/synchronization"
+require "capybara-patch/threaded_server"
+require "capybara-patch/whitespace"
