f00b23192748811f9077bd87b24eaaf775622e4e
Thomas Walpole <twalpole@gmail.com>
2015-04-13 11:43:10 -0700

    disable another maximize test on travis for now - see issue #1493

--- lib/capybara/spec/session/window/window_spec.rb
+++ lib/capybara/spec/session/window/window_spec.rb
@@ -121,7 +121,7 @@ Capybara::SpecHelper.spec Capybara::Window, requires: [:windows] do
 
   describe '#maximize' do
     it 'should be able to maximize window', requires: [:windows, :js] do
-      pending "This test fails when run with Firefox on Travis for an unknown reason - setting to pending for now" if ENV['TRAVIS'] && @session.respond_to?(:mode) && (@session.mode == :selenium_focus)
+      pending "This test fails when run with Firefox on Travis - see issue #1493 - setting to pending for now" if ENV['TRAVIS'] && @session.respond_to?(:mode) && (@session.mode == :selenium_focus)
       screen_width, screen_height = @session.evaluate_script("[window.screen.availWidth, window.screen.availHeight];")
       window = @session.current_window
       window.resize_to(screen_width-100, screen_height-100)
@@ -132,6 +132,7 @@ Capybara::SpecHelper.spec Capybara::Window, requires: [:windows] do
     end
 
     it 'should switch to original window if invoked not for current window', requires: [:windows, :js] do
+      pending "This test fails when run with Firefox on Travis - see issue #1493 - setting to pending for now" if ENV['TRAVIS'] && @session.respond_to?(:mode) && (@session.mode == :selenium_focus)
       @other_window = @session.window_opened_by do
         @session.find(:css, '#openWindow').click
       end
