75084c59fc64bc2d43f54e09fcd97945755a81cf
Thomas Walpole <twalpole@gmail.com>
2016-04-05 09:33:19 -0700

    Deprecate save_and_open_page_path in favor of save_path

--- lib/capybara/spec/session/save_screenshot_spec.rb
+++ lib/capybara/spec/session/save_screenshot_spec.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 Capybara::SpecHelper.spec '#save_screenshot', requires: [:screenshot] do
+  let(:alternative_path) { File.join(Dir.pwd, "save_screenshot_tmp") }
   before do
     @session.visit '/foo'
+  end
+
+  after do
+    Capybara.save_and_open_page_path = nil
+    Capybara.save_path = nil
+    FileUtils.rm_rf alternative_path
   end
 
   it "generates sensible filename" do
@@ -19,6 +26,28 @@ Capybara::SpecHelper.spec '#save_screenshot', requires: [:screenshot] do
     custom_path = 'screenshots/1.png'
     @session.save_screenshot(custom_path)
 
-    expect(@session.driver).to have_received(:save_screenshot).with(custom_path, {})
+    expect(@session.driver).to have_received(:save_screenshot).with(/#{custom_path}$/, {})
+  end
+
+  context "with Capybara.save_path" do
+    it "file is generated in the correct location" do
+      Capybara.save_path = alternative_path
+      allow(@session.driver).to receive(:save_screenshot)
+
+      @session.save_screenshot
+
+      regexp = Regexp.new(File.expand_path('capybara-\d+\.png', alternative_path))
+      expect(@session.driver).to have_received(:save_screenshot).with(regexp, {})
+    end
+
+    it "relative paths are relative to save_path" do
+      Capybara.save_path = alternative_path
+      allow(@session.driver).to receive(:save_screenshot)
+
+      custom_path = 'screenshots/2.png'
+      @session.save_screenshot(custom_path)
+
+      expect(@session.driver).to have_received(:save_screenshot).with(File.expand_path(custom_path, alternative_path), {})
+    end
   end
 end
