278cc7de2e729c0b967c567ed54712739c71f54b
Thomas Walpole <twalpole@gmail.com>
2017-01-03 01:51:01 -0800

    Fix type [ci skip]

--- lib/capybara/node/document_matchers.rb
+++ lib/capybara/node/document_matchers.rb
@@ -7,7 +7,7 @@ module Capybara
       #
       # @!macro title_query_params
       #   @overload $0(string, options = {})
-      #     @param string [String]           The string that title should include      #
+      #     @param string [String]           The string that title should include
       #   @overload $0(regexp, options = {})
       #     @param regexp [Regexp]           The regexp that title should match to
       #   @option options [Numeric] :wait (Capybara.default_max_wait_time) Maximum time that Capybara will wait for title to eq/match given string/regexp argument
