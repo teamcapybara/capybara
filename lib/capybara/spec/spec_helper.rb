f8d1caa66cbecf38cf05ee6c0384c6fae2a1aebe
Thomas Walpole <twalpole@gmail.com>
2017-11-14 20:17:11 -0800

    allow SpecHelper#spec to take multiple types of options

--- lib/capybara/spec/spec_helper.rb
+++ lib/capybara/spec/spec_helper.rb
@@ -52,7 +52,7 @@ module Capybara
         end
       end
 
-      def spec(name, options={}, &block)
+      def spec(name, *options, &block)
         @specs ||= []
         @specs << [name, options, block]
       end
@@ -79,7 +79,7 @@ module Capybara
           end
 
           specs.each do |spec_name, spec_options, block|
-            describe spec_name, spec_options do
+            describe spec_name, *spec_options do
               class_eval(&block)
             end
           end
