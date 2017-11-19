e7ed2a12bec27ff80fafa5904b99a2a988717512
Thomas Walpole <twalpole@gmail.com>
2017-11-19 10:06:18 -0800

    comment update [skip ci]

--- lib/capybara/session.rb
+++ lib/capybara/session.rb
@@ -261,10 +261,8 @@ module Capybara
 
           visit_uri_parts = visit_uri.to_hash.delete_if { |k,v| v.nil? }
 
-          # TODO - this is only for compatability with previous 2.x behavior that concatenated
-          # Capybara.app_host and a "relative" path - Consider removing in 3.0
-          # @abotalov brought up a good point about this behavior potentially being useful to people
-          # deploying to a subdirectory and/or single page apps where only the url fragment changes
+          # Useful to people deploying to a subdirectory
+          # and/or single page apps where only the url fragment changes
           visit_uri_parts[:path] = uri_base.path + visit_uri.path
 
           visit_uri = uri_base.merge(visit_uri_parts)
