e9ba14b44aa6c490e08c8e0044ba9fa954b4a263
Ken Collins <ken@metaskills.net>
2018-01-31 07:51:31 -0500

    Add No Sandbox to Selenium Chrome Headless
    In some non-Rails projects I like using Capybara defaults and testing on TravisCI. However, I found this memory issue to be an ever constant problem https://github.com/travis-ci/travis-ci/issues/7150
    
    Can we add `--no-sandbox` to the default options here for the `:selenium_chrome_headless` driver? Are there drawbacks to this?

--- lib/capybara.rb
+++ lib/capybara.rb
@@ -478,5 +478,6 @@ Capybara.register_driver :selenium_chrome_headless do |app|
   browser_options = ::Selenium::WebDriver::Chrome::Options.new
   browser_options.args << '--headless'
   browser_options.args << '--disable-gpu'
+  browser_options.args << '--no-sandbox'
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
 end
