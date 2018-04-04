88814620c9007f08ee20f7c3dee27017a2dc0774
Thomas Walpole <twalpole@gmail.com>
2018-04-04 12:03:21 -0700

    Only reset windows for browsers known to work

--- lib/capybara/selenium/driver.rb
+++ lib/capybara/selenium/driver.rb
@@ -109,8 +109,10 @@ class Capybara::Selenium::Driver < Capybara::Driver::Base
     # Use instance variable directly so we avoid starting the browser just to reset the session
     return unless @browser
 
-    switch_to_window(window_handles.first)
-    window_handles.slice(1..-1).each { |win| close_window(win) }
+    if firefox? || chrome?
+      switch_to_window(window_handles.first)
+      window_handles.slice(1..-1).each { |win| close_window(win) }
+    end
 
     navigated = false
     start_time = Capybara::Helpers.monotonic_time
