97ef0c9eb504e3af3a1fd43f3ebae7dc43e1c34d
Thomas Walpole <twalpole@gmail.com>
2018-07-10 16:58:08 -0700

    try sending :end instead of using setSelectionRange

--- lib/capybara/selenium/node.rb
+++ lib/capybara/selenium/node.rb
@@ -205,12 +205,12 @@ private
     elsif clear == :backspace
       # Clear field by sending the correct number of backspace keys.
       # Move caret to the end of the field before. Not supported on all input types (email for exemple).
-      if %w(text search password tel url).include?(self[:type])
-        caret_to_the_end = 'arguments[0].setSelectionRange(arguments[0].value.length, arguments[0].value.length)'
-        driver.execute_script caret_to_the_end, self
-      end
+      # if %w(text search password tel url).include?(self[:type])
+      #   caret_to_the_end = 'arguments[0].setSelectionRange(arguments[0].value.length, arguments[0].value.length)'
+      #   driver.execute_script caret_to_the_end, self
+      # end
       backspaces = [:backspace] * self.value.to_s.length
-      native.send_keys(*(backspaces + [value.to_s]))
+      native.send_keys(*([:end] + backspaces + [value.to_s]))
     elsif clear == :none
       native.send_keys(value.to_s)
     elsif clear.is_a? Array
