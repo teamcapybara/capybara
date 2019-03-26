d9b55926e06f5a5bff35141ee3fb9776a6e13057
Thomas Walpole <twalpole@gmail.com>
2019-03-26 09:29:10 -0700

    Fix warning in tests

--- lib/capybara/spec/session/find_spec.rb
+++ lib/capybara/spec/session/find_spec.rb
@@ -108,7 +108,7 @@ Capybara::SpecHelper.spec '#find' do
 
     it 'should warn if passed a non-valid locator type' do
       expect_any_instance_of(Kernel).to receive(:warn).with(/must respond to to_xpath or be an instance of String/)
-      expect { @session.find(:xpath, 123) }.to raise_error # rubocop:disable RSpec/UnspecifiedException
+      expect { @session.find(:xpath, 123) }.to raise_error Exception # The exact error is not yet well defined
     end
   end
 
