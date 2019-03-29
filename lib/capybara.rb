9fde708a45183e7851f1599e1c39e6a5889193d1
jeffclemens-ab <48690599+jeffclemens-ab@users.noreply.github.com>
2019-03-29 17:10:59 -0400

    Fix missing constant when using :selenium_chrome

--- lib/capybara.rb
+++ lib/capybara.rb
@@ -544,6 +544,7 @@ Capybara.register_driver :selenium_headless do |app|
 end
 
 Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.load_selenium
   browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
     opts.args << '--disable-site-isolation-trials'
