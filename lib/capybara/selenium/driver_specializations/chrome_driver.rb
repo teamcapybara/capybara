3b8c82d74fb4535178206aac324ae0eb1447c049
Thomas Walpole <twalpole@gmail.com>
2019-04-17 18:04:03 -0700

    Suppress some selenium-webdriver deprecation notices

--- lib/capybara/selenium/driver_specializations/chrome_driver.rb
+++ lib/capybara/selenium/driver_specializations/chrome_driver.rb
@@ -40,9 +40,15 @@ private
 
   def delete_all_cookies
     execute_cdp('Network.clearBrowserCookies')
-  rescue Selenium::WebDriver::Error::UnhandledError, Selenium::WebDriver::Error::WebDriverError
+  rescue *cdp_unsupported_errors
     # If the CDP clear isn't supported do original limited clear
     super
+  end
+
+  def cdp_unsupported_errors
+    ::Selenium::WebDriver.logger.suppress_deprecations do
+      [Selenium::WebDriver::Error::UnhandledError, Selenium::WebDriver::Error::WebDriverError]
+    end
   end
 
   def execute_cdp(cmd, params = {})
