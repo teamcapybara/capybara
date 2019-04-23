d6f570ea19c76b4c96300b1e6f0012d303fd2082
Thomas Walpole <twalpole@gmail.com>
2019-04-22 17:43:02 -0700

    Split selector definition and configured selector (#2178)
    * Split selector definition and configured selector

--- lib/capybara/selector/css.rb
+++ lib/capybara/selector/css.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'capybara/selector/selector'
+
 module Capybara
   class Selector
     class CSS
--- lib/capybara/spec/session/find_spec.rb
+++ lib/capybara/spec/session/find_spec.rb
@@ -235,7 +235,7 @@ Capybara::SpecHelper.spec '#find' do
 
   it 'should accept an XPath instance' do
     @session.visit('/form')
-    @xpath = Capybara::Selector[:fillable_field].call('First Name')
+    @xpath = Capybara::Selector.new(:fillable_field, config: {}).call('First Name')
     expect(@xpath).to be_a(::XPath::Union)
     expect(@session.find(@xpath).value).to eq('John')
   end
