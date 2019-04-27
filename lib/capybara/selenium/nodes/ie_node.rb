5a291b6db55250090eb2601d7d8810a3fa5b7fcc
Thomas Walpole <twalpole@gmail.com>
2019-04-27 09:48:42 -0700

    Revert disabled? optimization for IE

--- lib/capybara/selenium/nodes/ie_node.rb
+++ lib/capybara/selenium/nodes/ie_node.rb
@@ -4,6 +4,8 @@ require 'capybara/selenium/extensions/html5_drag'
 
 class Capybara::Selenium::IENode < Capybara::Selenium::Node
   def disabled?
-    driver.evaluate_script("arguments[0].msMatchesSelector(':disabled, select:disabled *')", self)
+    # TODO: Doesn't work for a bunch of cases - need to get IE running to see if it can be done like this
+    # driver.evaluate_script("arguments[0].msMatchesSelector(':disabled, select:disabled *')", self)
+    super
   end
 end
