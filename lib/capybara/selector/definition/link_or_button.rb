0ff52eb76bf4955a956d9b62f316f4093775dc33
Thomas Walpole <twalpole@gmail.com>
2019-05-14 20:07:21 -0700

    Default :link_or_button to href: false

--- lib/capybara/selector/definition/link_or_button.rb
+++ lib/capybara/selector/definition/link_or_button.rb
@@ -3,9 +3,8 @@
 Capybara.add_selector(:link_or_button, locator_type: [String, Symbol]) do
   label 'link or button'
   xpath do |locator, **options|
-    %i[link button].map do |selector|
-      expression_for(selector, locator, **options)
-    end.reduce(:union)
+    expression_for(:link, locator, **options.merge(href: false))
+      .union(expression_for(:button, locator, **options))
   end
 
   node_filter(:disabled, :boolean, default: false, skip_if: :all) { |node, value| !(value ^ node.disabled?) }
