9b356e61e1109ea3c6e8de2dd456c892bdfd5d99
toshimaru <me@toshimaru.net>
2019-08-30 01:03:33 +0900

    Add `no-sandbox` option for chrome_headless

--- lib/capybara/registrations/drivers.rb
+++ lib/capybara/registrations/drivers.rb
@@ -28,6 +28,7 @@ Capybara.register_driver :selenium_chrome_headless do |app|
   Capybara::Selenium::Driver.load_selenium
   browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
     opts.args << '--headless'
+    opts.args << "--no-sandbox"
     opts.args << '--disable-gpu' if Gem.win_platform?
     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
     opts.args << '--disable-site-isolation-trials'
