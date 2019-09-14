71e8213395c0c26c308618a574c409b1243bc585
Thomas Walpole <twalpole@gmail.com>
2019-09-14 11:23:49 -0700

    change to force travis

--- spec/selenium_spec_firefox.rb
+++ spec/selenium_spec_firefox.rb
@@ -71,6 +71,7 @@ Capybara::SpecHelper.run_specs TestSessions::SeleniumFirefox, 'selenium', capyba
   end
 end
 
+
 RSpec.describe 'Capybara::Session with firefox' do # rubocop:disable RSpec/MultipleDescribes
   include Capybara::SpecHelper
   ['Capybara::Session', 'Capybara::Node', Capybara::RSpecMatchers].each do |examples|
