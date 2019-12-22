ff56903eaed5266111a3166c7e72b1b33bfce997
Thomas Walpole <twalpole@gmail.com>
2019-12-22 09:37:42 -0800

    Skip accept_confirm nested modal test in FF

--- spec/selenium_spec_firefox.rb
+++ spec/selenium_spec_firefox.rb
@@ -59,6 +59,7 @@ Capybara::SpecHelper.run_specs TestSessions::SeleniumFirefox, 'selenium', capyba
   when 'Capybara::Session selenium #accept_confirm should work with nested modals'
     skip 'Broken in 63 <= FF < 69  - https://bugzilla.mozilla.org/show_bug.cgi?id=1487358' if firefox_gte?(63, @session) && firefox_lt?(69, @session)
     skip 'Hangs in 69 <= FF < 71  - Dont know what issue for this - previous issue was closed as fixed but it is not' if firefox_gte?(69, @session) && firefox_lt?(71, @session)
+    skip 'Broken again intermittently in FF 71 - jus skip it'
   when 'Capybara::Session selenium #click_link can download a file'
     skip 'Need to figure out testing of file downloading on windows platform' if Gem.win_platform?
   when 'Capybara::Session selenium #reset_session! removes ALL cookies'
