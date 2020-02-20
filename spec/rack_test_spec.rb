4b320fccd0be3d7f3b76095b42e98d540f501786
Thomas Walpole <twalpole@gmail.com>
2020-02-19 18:48:06 -0800

    Remove extraneous output from tests

--- spec/rack_test_spec.rb
+++ spec/rack_test_spec.rb
@@ -131,14 +131,12 @@ RSpec.describe Capybara::Session do # rubocop:disable RSpec/MultipleDescribes
         it 'should rewrite the forms action query for get submission' do
           session.visit('/form')
           session.click_button('mediocre')
-          puts session.current_url
           expect(session).not_to have_current_path(/foo|bar/)
         end
 
         it 'should rewrite the submit buttons formaction query for get submission' do
           session.visit('/form')
           session.click_button('mediocre2')
-          puts session.current_url
           expect(session).not_to have_current_path(/foo|bar/)
         end
       end
