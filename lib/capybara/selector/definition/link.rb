da55958746df3dcc3eae7ae238c15fba30e9f90d
Thomas Walpole <twalpole@gmail.com>
2020-04-05 22:43:33 -0700

    Add link arai role

--- lib/capybara/selector/definition/link.rb
+++ lib/capybara/selector/definition/link.rb
@@ -5,6 +5,13 @@ Capybara.add_selector(:link, locator_type: [String, Symbol]) do
     xpath = XPath.descendant(:a)
     xpath = builder(xpath).add_attribute_conditions(href: href) unless href == false
 
+    if enable_aria_role
+      role_path = XPath.descendant[XPath.attr(:role).equals('link')]
+      role_path = builder(role_path).add_attribute_conditions(href: href) unless [true, false].include? href
+
+      xpath += role_path
+    end
+
     unless locator.nil?
       locator = locator.to_s
       matchers = [XPath.attr(:id) == locator,
@@ -18,6 +25,7 @@ Capybara.add_selector(:link, locator_type: [String, Symbol]) do
 
     xpath = xpath[find_by_attr(:title, title)]
     xpath = xpath[XPath.descendant(:img)[XPath.attr(:alt) == alt]] if alt
+
     xpath
   end
 
