6440e5c76c77266d3546afe3e5f826992351cfca
Thomas Walpole <twalpole@gmail.com>
2021-01-20 14:25:07 -0800

    add_argument is available in all supported selenium-webdriver versions

--- lib/capybara/registrations/drivers.rb
+++ lib/capybara/registrations/drivers.rb
@@ -12,11 +12,7 @@ Capybara.register_driver :selenium_headless do |app|
   version = Capybara::Selenium::Driver.load_selenium
   options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
   browser_options = ::Selenium::WebDriver::Firefox::Options.new.tap do |opts|
-    if browser_options.respond_to?(:headless!)
-      opts.headless!
-    else
-      opts.args << '-headless'
-    end
+    opts.add_argument '-headless'
   end
   Capybara::Selenium::Driver.new(app, **Hash[:browser => :firefox, options_key => browser_options])
 end
@@ -26,11 +22,7 @@ Capybara.register_driver :selenium_chrome do |app|
   options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
   browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
-    if opts.respond_to?(:add_argument)
-      opts.add_argument('--disable-site-isolation-trials')
-    else
-      opts.args << '--disable-site-isolation-trials'
-    end
+    opts.add_argument('--disable-site-isolation-trials')
   end
 
   Capybara::Selenium::Driver.new(app, **Hash[:browser => :firefox, options_key => browser_options])
@@ -40,14 +32,10 @@ Capybara.register_driver :selenium_chrome_headless do |app|
   version = Capybara::Selenium::Driver.load_selenium
   options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
   browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
-    if opts.respond_to?(:headless!)
-      opts.headless!
-    else
-      opts.args << '--headless'
-    end
-    opts.args << '--disable-gpu' if Gem.win_platform?
+    opts.add_argument('--headless')
+    opts.add_argument('--disable-gpu') if Gem.win_platform?
     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
-    opts.args << '--disable-site-isolation-trials'
+    opts.add_argument('--disable-site-isolation-trials')
   end
 
   Capybara::Selenium::Driver.new(app, **Hash[:browser => :firefox, options_key => browser_options])
