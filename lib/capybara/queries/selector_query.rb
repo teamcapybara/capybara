33b9d229e0c7715c2484e9aa8e4884e586d956f7
ydah <13041216+ydah@users.noreply.github.com>
2022-09-22 11:03:54 +0900

    s/visbile/visible/

--- lib/capybara/queries/selector_query.rb
+++ lib/capybara/queries/selector_query.rb
@@ -570,7 +570,7 @@ module Capybara
         when :visible
           node.initial_cache[:visible] || (node.initial_cache[:visible].nil? && node.visible?)
         when :hidden
-          (node.initial_cache[:visible] == false) || (node.initial_cache[:visbile].nil? && !node.visible?)
+          (node.initial_cache[:visible] == false) || (node.initial_cache[:visible].nil? && !node.visible?)
         else
           true
         end
