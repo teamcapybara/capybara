2173bfeffe6997caad8b30ab39c65cf40bc31a0c
dsantosmerino <dsantosmerino@gmail.com>
2022-11-04 10:46:14 +0100

    Fix `Driver#submit` to forward the required args

--- lib/capybara/rack_test/driver.rb
+++ lib/capybara/rack_test/driver.rb
@@ -50,8 +50,8 @@ class Capybara::RackTest::Driver < Capybara::Driver::Base
     browser.refresh
   end
 
-  def submit(method, path, attributes)
-    browser.submit(method, path, attributes)
+  def submit(...)
+    browser.submit(...)
   end
 
   def follow(method, path, **attributes)
