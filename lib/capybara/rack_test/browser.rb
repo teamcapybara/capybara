0bbed8de38491a6bd52b405a83818486e853dbd7
Jeremy Evans <code@jeremyevans.net>
2023-08-18 16:17:42 -0700

    Attempt to appease Rubocop

--- lib/capybara/rack_test/browser.rb
+++ lib/capybara/rack_test/browser.rb
@@ -35,7 +35,7 @@ class Capybara::RackTest::Browser
     path = request_path if path.nil? || path.empty?
     uri = build_uri(path)
     uri.query = '' if method.to_s.casecmp('get').zero?
-    env = {'HTTP_REFERER' => referer_url}
+    env = { 'HTTP_REFERER' => referer_url }
     env['CONTENT_TYPE'] = content_type if content_type
     process_and_follow_redirects(
       method,
